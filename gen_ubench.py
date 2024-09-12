1d3228abab3bbd8350980b8f833da9186776751a
Max Hoffer <mhoffer@frontend.benchmarkcenter.megware.com>
2024-09-12 15:34:17 +0200

    only one asm

--- gen_ubench.py
+++ gen_ubench.py
@@ -2,9 +2,9 @@ file1 = open("ubench.cpp", "a")
 file2 = open("data/header.c", "r")
 file3 = open("data/footer.c", "r")
 
-instruction = "s_add_u32"
-register_type = "s"
-nb_register = 255
+instruction = "v_mul_f64"
+register_type = "v"
+nb_register = 128
 
 for line in file2:
     file1.write(line)
@@ -13,16 +13,38 @@ print("End of header")
 
 
 for i in range(nb_register):
-    file1.write("uint32_t register" + str(i) + " = 12;\n")
+    #file1.write("register float register" + str(i) + " asm(\"" + register_type + str(i) + "\");\n")
+    file1.write("float register" + str(i) + ";\n")
+    file1.write("register" + str(i) + " = " + str(i) + ";\n")
 
 
+
+file1.write("__asm__ volatile(\"s_waitcnt vmcnt(0)\");")
 
 
 file1.write("\nstart = clock();\n")
-
-for i in range(0, nb_register-1, 2):
-    file1.write("__asm__ volatile(\"" + instruction + " %0, %1, %2;\": \"=" + register_type + "\"(register" + str(i) + ") : \""+ register_type + "\"(register" + str(i+1) + ") , \""+ register_type + "\"(register" + str(i) + "));")
+#file1:write("for(int i = 0; i < 1000; i++){")
+file1.write("__asm__ volatile(")
+j = 0
+#Writing the instructions
+for i in range(0, (nb_register), 3):
+    #file1.write("__asm__ volatile(\"" + instruction + " %0, %1, %2;\": \"=" + register_type + "\"(register" + str(i) + ") : \""+ register_type + "\"(register" + str(i+1) + ") , \""+ register_type + "\"(register" + str(i) + "));")
+    file1.write("\"" + instruction + " ")
+    file1.write(" %" + str(i) + " ,%" + str(i+1) + " ,%" + str(i+2) + "\"\n\t") 
     file1.write("\n")
+
+file1.write(":")
+
+#writing the out register
+for i in range(0, (nb_register), 2):
+    file1.write("\"=" + register_type + "\"(register" + str(i) + "),\n" )
+
+file1.write(":")
+for i in range(0, (nb_register), 2):
+    file1.write("\"=" + register_type + "\"(register" + str(i+1) + "), \"=" + register_type + "\"(register" + str(i) + "),\n")
+
+file1.write(")\n\n")
+
 
 
 for line in file3:
