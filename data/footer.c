090a0ccd11204d5f246b25bb5f618b8dae1e90f3
Max Hoffer <mhoffer@gpu01sas.benchmarkcenter.megware.com>
2024-10-11 15:11:20 +0200

    Fix footer

--- data/footer.c
+++ data/footer.c
@@ -25,25 +25,23 @@ int main(void)
 	hipMalloc(&d_a, sizeof(uint32_t));
 	hipMalloc(&d_b, sizeof(uint32_t));
 
-	
 	clock_t *totalTime = (clock_t*)malloc(sizeof(clock_t));
 	*totalTime = 0;
 	clock_t *totalTime_d;
 	hipMalloc(&totalTime_d, sizeof(clock_t));
 
-	hipMemcpy(totalTime, totalTime_d, sizeof(clock_t), hipMemcpyDeviceToHost);
+	hipMemcpy(totalTime_d, totalTime, sizeof(clock_t), hipMemcpyDeviceToHost);
+	hipDeviceSynchronize();
 
-	for(int i =0; i < 10000; i++)
+	for(int i =0; i < 1000; i++)
 		asm_add<<<1, 1, 0,  0>>>(totalTime_d); 
-//	checkHipError(hipLaunchKernelGGL(asm_add, 1, 1, 0, 0,  d_a, d_b, totalTime_d), "error"); 
 
-	//hipDeviceSynchronize();
-	hipMemcpy(totalTime, totalTime_d, sizeof(clock_t), hipMemcpyDeviceToHost);
-	hipMemcpy(h_b, d_b, sizeof(uint32_t), hipMemcpyDeviceToHost);
+	hipDeviceSynchronize();
+	hipMemcpy(totalTime, totalTime_d, sizeof(clock_t), hipMemcpyHostToDevice);
+
+	printf("number of cycle = %d \n", *totalTime);
 
 
-	printf("b = %d", *h_b);
-	printf("time = %ld \n", (double)(*totalTime/NBREP));
 	free(totalTime);
 	free(h_b);
 	hipFree(totalTime_d);
